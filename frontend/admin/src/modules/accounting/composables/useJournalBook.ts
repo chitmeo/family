7ff7bc6da91513cf0d778acf2abcd6a7db03887a
cle-ecourier <cle@e-courier.com>
2025-11-13 23:06:14 +0700

    refactor(accounting): improve formatting and structure in useJournalBook composable

--- frontend/admin/src/modules/accounting/composables/useJournalBook.ts
+++ frontend/admin/src/modules/accounting/composables/useJournalBook.ts
@@ -1,35 +1,47 @@
 import { api } from "@chitmeo/shared";
 import { ref } from "vue";
-
+import type { JournalBook } from "@/modules/accounting/types";
 const loading = ref(false)
 const error = ref<string | null>(null)
 
 async function getBooks(chartOfAccountId: string, startDate: Date, endDate: Date) {
-  loading.value = true
-  error.value = null
-  try {
-    const res = await api.privateApi.post(`/accg/journalBook/getByDateRanges`, {
-      chartOfAccountId: chartOfAccountId,
-      startDate: startDate,
-      endDate: endDate
-    })
-    return res.data
-  } catch (err: any) {
-    error.value = api.extractApiError(err)
-  } finally {
-    loading.value = false
-  }
+    loading.value = true
+    error.value = null
+    try {
+        const res = await api.privateApi.post(`/accg/journalBook/getByDateRanges`, {
+            chartOfAccountId: chartOfAccountId,
+            startDate: startDate,
+            endDate: endDate
+        })
+        return res.data
+    } catch (err: any) {
+        error.value = api.extractApiError(err)
+    } finally {
+        loading.value = false
+    }
 }
 
-function toApiDateString(date: Date) {
-  const local = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-  return local.toISOString().split('T')[0]
+async function updateBook(book: JournalBook) {
+    loading.value = true
+    error.value = null
+    try {
+        const res = await api.privateApi.put(`accg/journalBook`)
+
+    }
+    catch (err: any) {
+        error.value = api.extractApiError(err)
+    }
+    finally {
+        loading.value = false
+    }
+
 }
+
 
 export function useJournalBook() {
-  return {
-    loading,
-    error,
-    getBooks
-  }
+    return {
+        loading,
+        error,
+        getBooks
+    }
 }
