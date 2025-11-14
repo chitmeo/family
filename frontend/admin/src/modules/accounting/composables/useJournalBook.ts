5b2622909fc06ba3d69611965e5eb43142c4ea0d
Kyle <kyle@chitmeo.com>
2025-11-14 10:27:22 +0700

    feat(accounting): implement create and update functionality for JournalBook with validation and UI enhancements

--- frontend/admin/src/modules/accounting/composables/useJournalBook.ts
+++ frontend/admin/src/modules/accounting/composables/useJournalBook.ts
@@ -1,5 +1,6 @@
-import { api } from "@chitmeo/shared";
 import { ref } from "vue";
+import { api } from "@chitmeo/shared";
+
 import type { JournalBook } from "@/modules/accounting/types";
 const loading = ref(false)
 const error = ref<string | null>(null)
@@ -25,8 +26,7 @@ async function updateBook(book: JournalBook) {
     loading.value = true
     error.value = null
     try {
-        const res = await api.privateApi.put(`accg/journalBook`)
-
+        await api.privateApi.put(`accg/journalBook/{${book.id}`, book)
     }
     catch (err: any) {
         error.value = api.extractApiError(err)
@@ -34,14 +34,43 @@ async function updateBook(book: JournalBook) {
     finally {
         loading.value = false
     }
-
 }
 
+async function createBook(book: JournalBook){
+    loading.value = true
+    error.value = null
+    try {
+        await api.privateApi.post(`accg/journalBook`, book)        
+    }
+    catch (err: any) {
+        error.value = api.extractApiError(err)
+    }
+    finally {
+        loading.value = false
+    }
+}
+
+async function deleteBook(id: string) {
+    loading.value = true
+    error.value = null
+    try {
+        await api.privateApi.delete(`accg/journalBook/${id}`)
+    }
+    catch (err: any) {
+        error.value = api.extractApiError(err)
+    }
+    finally {
+        loading.value = false
+    }
+}
 
 export function useJournalBook() {
     return {
         loading,
         error,
-        getBooks
+        createBook,
+        updateBook,
+        getBooks,
+        deleteBook    
     }
 }
